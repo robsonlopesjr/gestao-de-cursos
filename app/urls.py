74120c8fe8222d41f8d40259a77fc99a155040f8
Robson José Lopes G. Júnior <robsonlopes.jr@gmail.com>
2025-10-29 17:49:39 -0300

    adicionado personalização na página 404

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+# Carregando página 404 personalizada
+handler404 = "app.views.custom_page_not_found"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("courses.urls")),
